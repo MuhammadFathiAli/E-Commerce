bef0a5f80492ee09831714af40a4951ed556965b
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2022-05-05 21:26:40 +0200

    Product Configuration Class

--- Infrastructure/Migrations/20220505192609_add_Products_With_Configuration.Designer.cs
+++ Infrastructure/Migrations/20220505192609_add_Products_With_Configuration.Designer.cs
@@ -3,6 +3,7 @@ using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220505192609_add_Products_With_Configuration")]
+    partial class add_Products_With_Configuration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Infrastructure/StoreContext.cs
+++ Infrastructure/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +19,11 @@ namespace Infrastructure
         public virtual DbSet<Product> Products { get; set; }
         public  virtual DbSet<ProductBrand> ProductBrands { get; set; }
         public virtual DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());  
+        }
     }
 }
