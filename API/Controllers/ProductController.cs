d8689a51392ce57aac5cb7873081fdcc37767adb
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2022-05-05 17:39:46 +0200

    ProductController

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<List<Product>> products()
+        {
+            List<Product> products = _context.Products.ToList();
+
+          return products;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product> product(int id)
+        {
+            Product prd = _context.Products.Find(id);
+
+            return prd;
+        }
+    }
+}
---
+++ Infrastructure/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class StoreContext :DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
