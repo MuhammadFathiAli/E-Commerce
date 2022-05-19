283fcc44484b8378b99f6d1e43b738ee8baa5414
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2022-05-19 18:34:53 +0200

    update basketItem to align with product new props

--- Core/Entities/BasketItem.cs
+++ Core/Entities/BasketItem.cs
@@ -3,13 +3,12 @@
     public class BasketItem
     {
         public int Id { get; set; }
-        public string ProductName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public string PictureUrl { get; set; }
-        public string Brand { get; set; }
-        public string Type { get; set; }
-        
+        public string Image { get; set; }
+        public string Category { get; set; }        
 
     }
 }
